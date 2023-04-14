d710a779f1d658a985e365a602d054b280dc76d1
minthukyaw <minthukyaw454@gmail.com>
2023-04-14 20:46:18 +0630

    issue-1 branch created

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,8 @@
-import '@/styles/globals.css'
-import 'bootstrap/dist/css/bootstrap.css'
+import '@/styles/globals.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import "@fortawesome/fontawesome-svg-core/styles.css";
 import HeaderComponent from '@/components/HeaderComponent';
 import Navbar from '@/components/utils/Navbar';
-import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 
 config.autoAddCss = false; 
